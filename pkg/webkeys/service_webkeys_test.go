57e0e6e7aeaab705d52a8df24613f03067152743
JJ Philipp <jj.philipp@moov.io>
2020-05-22 15:18:19 -0500

    Adding testing to the webkeys package

---
+++ pkg/webkeys/service_webkeys_test.go
@@ -0,0 +1,145 @@
+package webkeys
+
+import (
+	"io/ioutil"
+	"net/http/httptest"
+	"os"
+	"syscall"
+	"testing"
+
+	"github.com/go-kit/kit/log"
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_GenerateKeys(t *testing.T) {
+	a, s := Setup(t)
+
+	config := WebKeysConfig{}
+	service, err := NewWebKeysService(s.logger, config)
+	a.Nil(err)
+
+	keys, err := service.FetchJwks()
+	a.Nil(err)
+	a.Len(keys.Keys, 1)
+
+	generator, ok := service.(*GenerateJwksService)
+	a.True(ok)
+
+	dir, err := ioutil.TempDir("", "test")
+	a.Nil(err)
+
+	generator.Save(dir)
+	t.Cleanup(func() {
+		syscall.Rmdir(dir)
+	})
+}
+
+func Test_HttpKeys(t *testing.T) {
+	a, s := Setup(t)
+
+	generator, err := NewGenerateJwksService()
+	a.Nil(err)
+
+	controller := NewJWKSController(generator)
+
+	router := mux.NewRouter()
+	controller.AppendRoutes(router)
+
+	server := httptest.NewServer(router)
+	t.Cleanup(server.Close)
+
+	// testing...
+	config := WebKeysConfig{
+		HTTP: &HttpConfig{
+			URL: server.URL + controller.WellKnownJwksPath(),
+		},
+	}
+
+	service, err := NewWebKeysService(s.logger, config)
+	a.Nil(err)
+
+	keys, err := service.FetchJwks()
+	a.Nil(err)
+
+	a.Len(keys.Keys, 1)
+}
+
+func Test_HttpKeys_NotFound(t *testing.T) {
+	a, s := Setup(t)
+
+	router := mux.NewRouter()
+	server := httptest.NewServer(router)
+	t.Cleanup(server.Close)
+
+	// testing...
+	config := WebKeysConfig{
+		HTTP: &HttpConfig{
+			URL: server.URL + "/.well-known/jwks.json",
+		},
+	}
+
+	service, err := NewWebKeysService(s.logger, config)
+	a.Nil(err)
+
+	_, err = service.FetchJwks()
+	a.NotNil(err)
+}
+
+func Test_FileKeys(t *testing.T) {
+	a, s := Setup(t)
+
+	generator, err := NewGenerateJwksService()
+	a.Nil(err)
+
+	dir, err := ioutil.TempDir("", "webkeystesting-"+uuid.New().String())
+	a.Nil(err)
+
+	generator.Save(dir)
+	t.Cleanup(func() {
+		syscall.Rmdir(dir)
+	})
+
+	// testing...
+	config := WebKeysConfig{
+		File: &FileConfig{
+			Path: dir + "/jwk-sig-pub.json",
+		},
+	}
+
+	service, err := NewWebKeysService(s.logger, config)
+	a.Nil(err)
+
+	keys, err := service.FetchJwks()
+	a.Nil(err)
+
+	a.Len(keys.Keys, 1)
+}
+
+func Test_FileKeys_NotFound(t *testing.T) {
+	a, s := Setup(t)
+
+	// testing...
+	config := WebKeysConfig{
+		File: &FileConfig{
+			Path: os.TempDir() + "/jwk-sig-pub.json",
+		},
+	}
+
+	service, err := NewWebKeysService(s.logger, config)
+	a.Nil(err)
+
+	_, err = service.FetchJwks()
+	a.NotNil(err)
+}
+
+type Scope struct {
+	logger log.Logger
+}
+
+func Setup(t *testing.T) (*assert.Assertions, Scope) {
+	return assert.New(t), Scope{
+		logger: log.NewNopLogger(),
+	}
+}
