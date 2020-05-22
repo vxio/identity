51c701f9fc2e66362d8f563b76b6d558049a9eb1
JJ Philipp <jj.philipp@moov.io>
2020-05-21 19:11:01 -0500

    Added in test for loading environment config

---
+++ pkg/identity/config_test.go
@@ -0,0 +1,19 @@
+package identity
+
+import (
+	"testing"
+
+	"github.com/go-kit/kit/log"
+	configpkg "github.com/moov-io/identity/pkg/config"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_ConfigLoading(t *testing.T) {
+	logger := log.NewNopLogger()
+
+	ConfigService := configpkg.NewConfigService(logger)
+
+	config := &Config{}
+	err := ConfigService.Load(config)
+	require.Nil(t, err)
+}
